bfdf7453f466ec2a515e53bcf165940a99efe30d
Gavin Costin <villa.straylite@proton.me>
2024-07-17 12:47:55 +0800

    added dummy test

---
+++ test/test.js
@@ -0,0 +1,9 @@
+// test/test.js
+
+const assert = require('assert');
+describe('BCH dummy test', function() {
+  it('should return true', function() {
+    assert.strictEqual(true, true);
+  });
+});
+
